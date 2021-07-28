750fd8202a9cac78a7cd29c625d8f3a3b5056fec
Varun Sharma <varunbkn1210@gmail.com>
2021-07-28 19:01:01 +0530

    New Changes

--- CodeForces/HarbourDiv2B.cpp
+++ CodeForces/HarbourDiv2B.cpp
@@ -5,49 +5,87 @@ const ll mod = 1000000007;
 #define send {ios_base::sync_with_stdio(false);}
 #define help {cin.tie(NULL);}
 #define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
-bool ok(string a,int idx, string b)
+int left(string s, string t, int i, int x)
 {
-    int x = a.size()-1;
-    for(int i = 1;i<b.size();i++)
+    while (x < t.length() && i >= 0)
     {
-        if( (idx > 0 && a[idx-1] == b[i]))
+        if (t[x] == s[i])
         {
-            --idx;
-        }
-        else if((idx < x && a[idx+1] == b[i]))
-        {
-            ++idx;
+            x++;
+            i--;
         }
         else
         {
-            return false;
+            break;
         }
     }
-    return true;
+    if (x == t.length())
+    {
+        return 1;
+    }
+    return 0;
+}
+ 
+int right(string s, string t, int i, int x)
+{
+    if (x == t.length())
+    {
+        return 1;
+    }
+    if (x + 1 == t.length())
+    {
+        if (i + 1 < s.length() && s[i + 1] == t[x])
+        {
+            return 1;
+        }
+        else if (i - 1 >= 0 && s[i - 1] == t[x])
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+ 
+    int r1 = 0, r2 = 0;
+    if (i - 1 >= 0 && s[i - 1] == t[x])
+    {
+        r1 = left(s, t, i - 1, x);
+    }
+    if (r1)
+    {
+        return 1;
+    }
+    if (i + 1 < s.length() && s[i + 1] == t[x])
+    {
+        r2 = right(s, t, i + 1, x + 1);
+    }
+ 
+    return (r1 || r2);
 }
 void solve()
 {    
     string a,b;
     cin>>a>>b;
-    int i = 0;
-    vector<int>ans;
-    for(int i=0;i<a.size();i++)
+    int n = a.size();
+    int f = 0;
+    for(int i=0;i<n;i++)
     {
-        if(b[0] == a[i])
+        if(a[i] == b[0])
         {
-            ans.push_back(i); 
+           f = right(a,b,i,1);
+           if(f==1){
+               break;
+           }
         }
-    }
-    string res = "NO";
-    for(auto i : ans)
+    }    
+    if(f)
     {
-        if(ok(a,i,b))
-        {
-            res = "YES";
-            break;
-        }
-    }
-    cout<<res<<endl;
+        cout<<"YES"<<endl;
+    }   
+    else
+    cout<<"NO"<<endl;
 }
 int main()
 {   
