745720972bf3fe62080687ac6aa54049b156d306
Varun Sharma <varunbkn1210@gmail.com>
2021-11-02 19:39:45 +0530

    New Changes

---
+++ CodeForces/752Div2D.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007;
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+void solve()
+{    
+    int x,y;
+    cin>>x>>y;
+    if(x > y)
+    {
+        cout<<x + y <<endl;
+    }       
+    else
+    {
+        cout<<y - (y % x)/2<<endl;
+    }
+}
+int main()
+{   
+    send help
+    cout << setprecision(15) << fixed;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
